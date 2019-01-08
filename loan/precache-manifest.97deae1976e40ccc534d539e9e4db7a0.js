3cfc6428735c7d381836f4a73c83cd1db4de6791
Steven Laidlaw <steven.laidlaw@seqta.com.au>
2019-01-08 19:51:19 +0800

    Added loan calculator

---
+++ loan/precache-manifest.97deae1976e40ccc534d539e9e4db7a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5994cd06b7724d4f55f5",
+    "url": "/static/js/main.5994cd06.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "5994cd06b7724d4f55f5",
+    "url": "/static/css/main.54e65dc6.chunk.css"
+  },
+  {
+    "revision": "e5bd6a614437695f53f9a93b161441d1",
+    "url": "/index.html"
+  }
+];
