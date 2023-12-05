c5afd4c0f150b622b728a1f287a336b3e7f6f949
Steven Laidlaw <steven@stevenlaidlaw.com>
2023-12-05 12:18:23 +0800

    Tue Dec  5 12:18:23 AWST 2023

--- src/generate.js
+++ src/generate.js
@@ -2,11 +2,6 @@ const fs = require("fs");
 
 const run = () => {
   const layoutContent = fs.readFileSync("./content/layout.html", "utf8");
-  // Generate the index page
-  // const sections = fs.readdirSync("./sections").map((section) => `<div class="section">${fs.readFileSync("./sections/" + section, "utf-8")}</div>`);
-
-  // // const html = layoutContent.replace("{{sections}}", sections.join(""));
-  // fs.writeFileSync("../index.html", html);
   fs.writeFileSync("../index.html", layoutContent);
 };
 
