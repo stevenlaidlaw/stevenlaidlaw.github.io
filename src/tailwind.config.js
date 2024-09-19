ffa652b23668f2f507577b47a36b0eee483e9321
Steven Laidlaw <steven@stevenlaidlaw.com>
2024-09-19 23:03:43 +0800

    Thu Sep 19 23:03:42 AWST 2024

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -17,6 +17,25 @@ export default {
       ],
       header: ["Nova Square", "Lexend Deca", "sans-serif"],
     },
+    extend: {
+      borderWidth: {
+        16: "16px",
+      },
+      animation: {
+        gradBg: "gradBg 18s linear infinite",
+        gradBorder: "gradBorder 18s linear infinite",
+      },
+      keyframes: {
+        gradBg: {
+          "0%, 100%": { backgroundPosition: "0% 50%" },
+          "50%": { backgroundPosition: "100% 50%" },
+        },
+        gradBorder: {
+          "0%, 100%": { '--angle': "45deg" },
+          "50%": { '--angle': "360deg" },
+        },
+      }
+    }
   },
   plugins: [],
 };
