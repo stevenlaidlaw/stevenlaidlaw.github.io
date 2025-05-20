b6794499bb1debe65ecb6301349ed8af6e6d3126
Steven Laidlaw <steven@stevenlaidlaw.com>
2025-05-20 20:00:42 +0800

    Tue May 20 20:00:42 AWST 2025

--- src/components/layout.js
+++ src/components/layout.js
@@ -29,11 +29,9 @@ const Layout = ({ children }) => {
         <div>
           <a href="https://github.com/stevenlaidlaw">github</a>
           {" // "}
-          <a rel="me" href="https://mastodon.social/@stevenlaidlaw">
-            Mastodon
-          </a>
-          {" // "}
           <a href="https://www.linkedin.com/in/stevenglaidlaw/">linkedin</a>
+          {" // "}
+          <a href="/rss">rss</a>
         </div>
       </header>
       <main>{children}</main>
